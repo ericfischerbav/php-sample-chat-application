b477f3051d64d52b489fc838c9e777dc1127af64
Eric Fischer <eric.fischer.bav@gmail.com>
2018-05-13 21:23:50 +0200

    First files added

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<html>
+	
+	<head>
+		<title>Chat Application</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	</head>
+	
+	<body>
+		<h1>Chat Application</h1>
+		<p>Bitte logge dich ein, um chatten zu k&ouml;nnen.</p>
+		<p><a href="login.php">Login</a></p>
+		
+		<script type="text/javascript" src="js/bootstrap.bundle.js">
+		</script>
+	</body>
+
+</html>
