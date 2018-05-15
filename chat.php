2aa45c2abbd61fa897f697054073f5b1a1d25052
Eric Fischer <eric.fischer.bav@gmail.com>
2018-05-15 08:45:38 +0200

    Started implementing the start chat func

--- chat.php
+++ chat.php
@@ -14,6 +14,38 @@ if(!isset($_POST["start-chat"]) or !isset($_POST["users"])) {
     exit();
 }
 
+include "internal/properties.inc.php";
+
+// DB-Verbindung öffnen
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+mysqli_select_db($connection, DB_NAME);
+
+/*
+ * Erstelle den Chat an der Datenbank.
+ * Das soll allerdings nur gemacht werden, wenn auch
+ * das "start-chat"-Flag gesetzt ist.
+ */
+$error_inserting = true;
+if(isset($_POST["start-chat"])) {
+    $users = $_POST["users"];
+    $chat_id = generate_chat_id();
+    foreach ($users as $user) {
+        $sql = "INSERT INTO nimmtteil (chat, benutzer) VALUES ('".$user."', ".$chat_id.")";
+        $db_result = mysqli_query($connection, $sql);
+        if($db_result == false) {
+            $error_inserting = true;
+            /*
+             * Alle Einträge löschen, die bisher geschrieben wurden.
+             * Das ist nur notwendig, da MySQL im Auto-Commit-Modus
+             * läuft. Wäre dieser deaktiviert, würde ein einfaches
+             * Rollback reichen.
+             */
+            
+            break;
+        }
+    }
+}
+
 /**
  * Creates the title that fits to the state of the page.
  */
@@ -24,6 +56,22 @@ function create_title()
     else 
         return "Chat";
 }
+
+function generate_chat_id() {
+    global $connection;
+    
+    $id = rand(0, 2147483646);
+    
+    $check_sql = "SELECT * FROM nimmtteil WHERE chat = ".$id;
+    $db_result = mysqli_query($connection, $check_sql);
+    if (mysqli_fetch_assoc($db_result)) {
+        return generate_chat_id();
+    } else {
+        return $id;
+    }
+}
+
+mysqli_close($connection);
 
 ?>
 
