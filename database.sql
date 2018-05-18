973d5ddadba04e730cbb1959ce0fecce1cc9a43a
Eric Fischer <eric.fischer.bav@gmail.com>
2018-05-18 09:29:38 +0200

    Added the create tables.

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE benutzer (
+	name VARCHAR(50) PRIMARY KEY,
+	passwort VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE nimmtteil (
+	benutzer VARCHAR(50),
+	chat INT,
+	PRIMARY KEY (benutzer, chat),
+	FOREIGN KEY (benutzer) REFERENCES benutzer(name)
+);
+
+CREATE TABLE nachricht (
+	id INT PRIMARY KEY,
+	text VARCHAR(600) NOT NULL,
+	zeit TIMESTAMP NOT NULL,
+	chat INT NOT NULL,
+	sender VARCHAR(50) NOT NULL,
+	FOREIGN KEY (sender) REFERENCES benutzer(name),
+	FOREIGN KEY (chat) REFERENCES nimmtteil(chat)
+);
